ba0032a77e4fbf1868c4205abf0e400eaa44a91e
Hyunwc <xogus565@naver.com>
2024-03-25 19:25:19 +0900

    textRPG commit

---
+++ TextRPG/GameManager.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "MapDraw.h"
+
+class GameManager
+{
+public:
+	
+};
---
+++ TextRPG/Mecro.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+#include <Windows.h>
+#include <conio.h>
+#include <string>
+#include <fstream> //파일 입출력
+#include <time.h>
+#include <list>
+using namespace std;
+
+#define col GetStdHandle(STD_OUTPUT_HANDLE)
+#define ORIGINAL SetConsoleTextAttribute( col,0x0007 );
+#define RED SetConsoleTextAttribute( col,0x000c );
+#define YELLOW SetConsoleTextAttribute( col,0x000e );
+
+#define WIDTH 30
+#define HEIGHT 30
